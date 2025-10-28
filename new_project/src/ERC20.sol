078696e2ae0b609054af049911753ecde95f1f43
0x11semprez <traoresemprez@icloud.com>
2025-10-28 16:53:14 +0100

    day11

--- new_project/src/ERC20.sol
+++ new_project/src/ERC20.sol
@@ -1,10 +1,19 @@
 pragma solidity >=0.8.0;
 
-import "./interface/IERC20.sol";
+import {IERC20} from "./interface/IERC20.sol";
 
-contract ERC20 {
+contract ERC20 is IERC20 {
     string public name = "AmentaSanctum";
     string public symbol = "AMS";
-    uint private decimals = 10;
-    uint public _totalSupply;
+    uint8 public decimals = 18;
+    uint private _totalSupply;
+
+    mapping(address account => uint256) private _balances;
+    mapping(address owner => mapping(address spender => uint256)) private _allowances;
+
+    constructor(uint private _totalSupply) {
+        _totalSupply = 10 * (10 ** uint256(decimals));
+        _balances[msg.sender] = _totalSupply;
+        event Transfert(address indexed from, adress indexed to, uint256 value);
+    }
 }
